a9fb31e6ce846d766b2e08b8ab6e0091d88791a2
ricardolml <ricardo.esp.vz@gmail.com>
2021-10-21 17:41:57 -0500

    initial setup

---
+++ src/app/producs/producs.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProducsRoutingModule } from './producs-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ProducsRoutingModule
+  ]
+})
+export class ProducsModule { }
